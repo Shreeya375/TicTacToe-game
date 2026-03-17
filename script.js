492be88964ced7781b7a2289ba544589d1a9cc16
Shreeya Mody <shreeyamody07@gmail.com>
2026-03-17 10:52:08 -0500

    start script.js
    initialized variables and combos for winning

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Tic Tac Toe game started");
+/* --- game data ---- */
+const board = Array(9).fill('');  // 3 x 3 tic tac toe board
+let currentPlayer = 'X';  // player X always goes first
+let gameOver = false;  // flag to track if the game has ended
+
+const END_Game = {
+    WIN: 'win',
+    TIE: 'tie',
+    NONE: 'none';
+};
+
+/* all winning  combos */
+const winningConditions = [
+    [0,1,2], [3,4,5], [6,7,8],  // rows
+    [0,3,6], [1,4,7], [2,5,8],  // columns
+    [0,4,8], [2,4,6]             // diagonals
+];
+
