dc956cd828789f9326612c9a5dbad9222e150622
Shreeya Mody <shreeyamody07@gmail.com>
2026-03-17 11:08:32 -0500

    debug html
    fixed most of the syntax errors

--- script.js
+++ script.js
@@ -16,3 +16,35 @@ const winningConditions = [
     [0,4,8], [2,4,6]             // diagonals
 ];
 
+/* references == */
+
+const tiles = Array.from(document.querySelectorAll('.tile'));  // all the tiles
+const announcement = document.getElementById('announcement');  // end game message 
+const resetButton = document.getElementById('reset');  // reset button
+const displayCurrentPlayer = document.getElementById('current-player');  // display current player
+
+/* --- event listeners -- */
+tiles.forEach((tile,idx))  => {
+    tile.addEventListener('click', () => userAction(tile, idx));
+};
+
+resetButton.addEventListener('click', resetGame);
+
+/* --- functions --- */
+
+function userAction(tile, index) {
+    if (!isValidAction(tile) || !isGameActive) return;  // ignore if game is over or tile is occupied 
+    tile.innerText = currentPlayer;  // mark the tile
+    tile.classList.add(currentPlayer);  // add class for styling
+    updateBoard(index);  // update the game state;
+    handleResultValidation();  // check for win/tie
+    if(isGameActive) changePlayer();  // switch turns if game is still active and not over
+}
+
+function isValidAction(tile) {
+    return tile.innerText === '';  // valid if the tile is empty
+}
+
+function updateBoard(index) {
+    board[index] = currentPlayer;  // update the board state
+}
